412fe4fa6e12d6d0a49efdbd5467e49dc4df0790
chelsymooy <chelsymooy1108@gmail.com>
2017-09-13 21:35:12 +0700

    #2 clean listener

--- thunderlabid/Pengajuan/Listeners/GenerateStatusPermohonan.php
+++ thunderlabid/Pengajuan/Listeners/GenerateStatusPermohonan.php
@@ -60,24 +60,4 @@ class GenerateStatusPermohonan
 
 		$status 		= Status::create($data);
 	}
-
-	protected function setIDPengajuan($model)
-	{
-		$first_letter       = Carbon::now()->format('ym').'.'.$model->kode_kantor.'.';
-		$prev_data          = Pengajuan::where('id', 'like', $first_letter.'%')->orderby('id', 'desc')->first();
-
-		if($prev_data)
-		{
-			$last_letter	= explode('.', $prev_data['id']);
-			$last_letter	= ((int)$last_letter[3] * 1) + 1;
-		}
-		else
-		{
-			$last_letter	= 1;
-		}
-
-		$last_letter		= str_pad($last_letter, 4, '0', STR_PAD_LEFT);
-
-		return $first_letter.$last_letter;
-	}
 }
