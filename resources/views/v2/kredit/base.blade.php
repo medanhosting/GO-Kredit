b2801b28ecb9d435d20085dc803eba35f447825f
chelsy <chelsymooy1108@gmail.com>
2017-12-30 10:32:04 +0700

    help base out and pelunasan angsuran

--- resources/views/v2/kredit/base.blade.php
+++ resources/views/v2/kredit/base.blade.php
@@ -3,8 +3,8 @@ px-3 py-1 my-3 {{ in_array(strtolower($active_submenu), ['kredit']) ? 'active' :
 	<i class="fa fa-credit-card-alt"></i>&nbsp;&nbsp;Kredit
 </a>
 <a href="{{route('jaminan.index', ['kantor_aktif_id' => $kantor_aktif_id])}}" class="nav-link px-3 py-1 my-3
-px-3 py-1 my-3 {{ in_array(strtolower($active_submenu), ['jaminan']) ? 'active' : '' }}">
-	<i class="fa fa-exchange"></i>&nbsp;&nbsp;Mutasi Jaminan
+px-3 py-1 my-3 {{ in_array(strtolower($active_submenu), ['angsuran']) ? 'active' : '' }}">
+	<i class="fa fa-hand-rock-o"></i>&nbsp;&nbsp;Penerimaan Angsuran
 </a>
 <a href="{{route('tunggakan.index', ['kantor_aktif_id' => $kantor_aktif_id])}}" class="nav-link px-3 py-1 my-3
 px-3 py-1 my-3 {{ in_array(strtolower($active_submenu), ['tunggakan']) ? 'active' : '' }}">
