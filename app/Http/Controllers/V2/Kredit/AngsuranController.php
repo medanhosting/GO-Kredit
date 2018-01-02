e5896b257c9375b277f2011a05afe4819cfecbc0
chelsy <chelsymooy1108@gmail.com>
2018-01-02 12:04:13 +0700

    save it

---
+++ app/Http/Controllers/V2/Kredit/AngsuranController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\V2\Kredit;
+
+use App\Http\Controllers\Controller;
+
+use Thunderlabid\Kredit\Models\NotaBayar;
+
+use Exception, Auth, Carbon\Carbon;
+
+class AngsuranController extends Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index() 
+	{
+		$today 		= Carbon::now();
+
+		if(request()->has('q')){
+			$today	= Carbon::createFromFormat('d/m/Y', request()->get('q'));
+		}
+
+		$angsuran 	= NotaBayar::wherehas('kredit', function($q){$q->where('kode_kantor', request()->get('kantor_aktif_id'));})->wherehas('details', function($q){$q;})->Displaying()->paginate();
+
+		view()->share('active_submenu', 'angsuran');
+		view()->share('kantor_aktif_id', request()->get('kantor_aktif_id'));
+
+		$this->layout->pages 	= view('v2.kredit.angsuran.index', compact('angsuran'));
+		return $this->layout;
+	}
+}
