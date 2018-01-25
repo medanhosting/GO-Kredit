c23cf429d83a75b8968f1358375ab6d1c87205c2
root <chelsymooy1108@gmail.com>
2018-01-25 13:13:11 +0700

    Turn off db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$this->call(IndonesiaTableSeeder::class);
+		// $this->call(IndonesiaTableSeeder::class);
 		// $this->call(LiveTableSeeder::class);
-		// $this->call(FinanceTableSeeder::class);
-		// $this->call(ManajemenTableSeeder::class);
-		// $this->call(PengajuanTableSeeder::class);
-		// $this->call(SurveiTableSeeder::class);
-		// $this->call(AnalisaTableSeeder::class);
-		// $this->call(PutusanTableSeeder::class);
-		// $this->call(KreditAktifTableSeeder::class);
+		$this->call(FinanceTableSeeder::class);
+		$this->call(ManajemenTableSeeder::class);
+		$this->call(PengajuanTableSeeder::class);
+		$this->call(SurveiTableSeeder::class);
+		$this->call(AnalisaTableSeeder::class);
+		$this->call(PutusanTableSeeder::class);
+		$this->call(KreditAktifTableSeeder::class);
 		// $this->call(SurveiTakePictureTableSeeder::class);
 		
 		// $this->call(PengajuanDuplicateTableSeeder::class);
